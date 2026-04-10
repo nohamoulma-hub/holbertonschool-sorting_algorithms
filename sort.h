855e5d95d6d3af2185a5be0d376775cf36889b8a
panaki-gillot <gillotpanaki@gmail.com>
2026-04-10 11:48:43 +0200

    Feat: Implement quick sort algorithm with recursive and partition functions

--- sort.h
+++ sort.h
@@ -17,6 +17,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void swap(int *a, int *b);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
+int partition(int *array, int low, int high, size_t size);
 void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
